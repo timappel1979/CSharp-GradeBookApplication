f61353a7348111daf9e133dd3e348a75215a4066
Tim Appel <tappel@chanllc.com>
2020-03-18 17:09:48 -0500

    check 1.1

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -19,6 +19,7 @@ namespace GradeBook.GradeBooks
         {
             Name = name;
             Students = new List<Student>();
+            Type = new GradeBookType();
         }
 
         public void AddStudent(Student student)
