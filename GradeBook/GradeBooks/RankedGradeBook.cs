e0ef73a8d3615bac4b01d8a6f37116ab48e4dda7
Tim Appel <tappel@chanllc.com>
2020-03-24 14:39:01 -0500

    2.1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,5 +9,30 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var studentCount = Students.Count;
+
+            if (studentCount < 5)
+                throw new System.InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            var gradeThreshhold = (int)Math.Ceiling(studentCount * 0.2);
+            var gradeOrder = Students.OrderByDecending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (gradeOrder[gradeThreshhold - 1] <= averageGrade)
+                return 'A';
+
+            else if (gradeOrder[(gradeThreshhold * 2) - 1] <= averageGrade)
+                return 'B';
+
+            else if (gradeOrder[(gradeThreshhold * 3) - 1] <= averageGrade)
+                return 'C';
+
+            else if (gradeOrder[(gradeThreshhold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
