10624d4dfe8f806bfa50df1ddd2e128e04f8f334
Tim Appel <tappel@chanllc.com>
2020-03-26 13:48:45 -0500

    2.13

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,14 +21,15 @@ namespace GradeBook.GradeBooks
             List<Student> students = new List<Student>();
 
             var studentCount = students.Count;
+            var gradeThreshhold = (int)Math.Ceiling(studentCount * 0.2);
+            var gradeOrder = students.OrderBy(e => e.AverageGrade).ToList();
+
 
             if (studentCount < 5)
                 throw new System.InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
-            var gradeThreshhold = (int)Math.Ceiling(studentCount * 0.2);
-            var gradeOrder = students.OrderBy(e => e.AverageGrade).ToList();
 
-            if (gradeOrder[gradeThreshhold - 1].AverageGrade <= averageGrade)
+            else if (gradeOrder[gradeThreshhold - 1].AverageGrade <= averageGrade)
                 return 'A';
             else if (gradeOrder[(gradeThreshhold * 2) - 1].AverageGrade <= averageGrade)
                 return 'B';
