6e936e89eb9fda563890bd6caff24b5c561ccab7
Tim Appel <tappel@chanllc.com>
2020-03-26 19:31:01 -0500

    3.3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -42,7 +42,7 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
         }
 
-        public override void CalculateStudentStatistics()
+        public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
                 System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
